68eb661540adead1e148d85f5bbd3c79309c4ce2
George Nairooze <george.nairooze@linkdev.com>
2019-06-11 17:30:45 +0200

    use information schema instead of sysobjects

--- read tables columns.sql
+++ read tables columns.sql
@@ -11,32 +11,34 @@ declare @tables table
 insert @tables (name) values ('Category')
 insert @tables (name) values ('Log')
 
-select
-	Table_Name = so.Name,
-	Column_Name = sc.Name,
-	--sc.ColOrder,
-	--sc.status,
-	Is_Primary = case 
-		when  (sc.status = 0x80) OR (sc.ColOrder = 1 and not sc.status = 0x80 ) then 1
+select distinct
+	table_name = C.TABLE_NAME,
+	column_name = C.COLUMN_NAME,
+	data_type = C.DATA_TYPE,
+	max_length = C.CHARACTER_MAXIMUM_LENGTH,
+		is_primary = case
+		when KU.CONSTRAINT_NAME is not null then 1
 		else 0
 		end,
-	[Type_Name] = st.name,
-	[Length] = sc.length,
-	[Is_Required] = case sc.isnullable
-		when 0 then 1
-		else 0
+	is_required = case C.IS_NULLABLE
+		when 'YES' then 0
+		else 1
 		end,
-	[Is_Computed] = sc.iscomputed,
-	[Collation] = sc.collation
-from sysobjects so
-inner join syscolumns sc
-	on so.id=  sc.id
-inner join systypes st
-	on sc.xusertype = st.xusertype
+	collation_name = C.COLLATION_NAME
+from INFORMATION_SCHEMA.COLUMNS as C
 inner join @tables tabs
-	on so.Name = tabs.name
-where 1=1
-and so.xtype = 'U' --user table
-order by
-	Table_Name,
-	Column_Name
+	on C.TABLE_NAME = tabs.name
+left join INFORMATION_SCHEMA.TABLE_CONSTRAINTS as TC
+	on C.TABLE_NAME = TC.TABLE_NAME
+	and C.TABLE_SCHEMA = TC.TABLE_SCHEMA
+	and C.TABLE_CATALOG = TC.TABLE_CATALOG
+left join INFORMATION_SCHEMA.KEY_COLUMN_USAGE as KU
+	on TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME 
+	and C.COLUMN_NAME = KU.COLUMN_NAME
+	and C.TABLE_NAME = KU.TABLE_NAME
+	and C.TABLE_SCHEMA = KU.TABLE_SCHEMA
+	and C.TABLE_CATALOG = KU.TABLE_CATALOG
+	and TC.CONSTRAINT_TYPE = 'PRIMARY KEY'
+order by 
+	table_name, 
+	column_name
